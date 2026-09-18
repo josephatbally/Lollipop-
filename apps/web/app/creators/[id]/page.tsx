c38326913982b9c3873d50449824e1d2854398bf
josephatbally <josephatbally5@gmail.com>
2026-09-18 23:40:43 +0300

    fix(web): correct API helper imports

--- apps/web/app/creators/[id]/page.tsx
+++ apps/web/app/creators/[id]/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 
-import { API, readJson } from "./api-client";
+import { API, readJson } from "../../api-client";
 
 type Plan = { price_cents: number; currency: string };
 type Creator = {
